fc91ba496343c3c57ee450f10ee918667f9aaa34
Alex Kyllo <jekyllo@microsoft.com>
2021-04-29 23:15:17 -0700

    save emacs config

---
+++ init.el
@@ -0,0 +1,77 @@
+; Manually load package instead of waiting until after init.el is loaded
+(package-initialize)
+; Disable loading package again after init.el
+(setq package-enable-at-startup nil)
+
+; Enable "package", for installing packages
+; Add some common package repositories
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/"))
+
+; Use "package" to install "use-package", a better package management and config system
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+; Set up elpy for Python in Emacs
+(use-package elpy
+  :ensure t
+  :pin elpy
+  :config
+  (elpy-enable)
+  ;; Enable elpy in a Python mode
+  (add-hook 'python-mode-hook 'elpy-mode)
+  (setq elpy-rpc-backend "jedi")
+  ;; Open the Python shell in a buffer after sending code to it
+  (add-hook 'inferior-python-mode-hook 'python-shell-switch-to-shell)
+  ;; Use IPython as the default shell, with a workaround to accommodate IPython 5
+  ;; https://emacs.stackexchange.com/questions/24453/weird-shell-output-when-using-ipython-5
+  (setq python-shell-interpreter "jupyter-console")
+  (setq python-shell-interpreter-args "--simple-prompt")
+  ;; Enable pyvenv, which manages Python virtual environments
+  (pyvenv-mode 1)
+  ;; Tell Python debugger (pdb) to use the current virtual environment
+  ;; https://emacs.stackexchange.com/questions/17808/enable-python-pdb-on-emacs-with-virtualenv
+  (setq gud-pdb-command-name "python -m pdb "))
+
+;; nord theme
+(use-package nord-theme :ensure t)
+(load-theme 'nord)
+
+;; neotree
+(use-package neotree :ensure t)
+(use-package all-the-icons :ensure t)
+(global-set-key [f8] 'neotree-toggle)
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
+;; ivy autocompletion
+(use-package ivy :ensure t)
+(ivy-mode)
+
+;; magit
+(use-package magit :ensure t)
+
+(global-linum-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" default)))
+ '(package-selected-packages
+   (quote
+    (magit ivy all-the-icons neotree use-package nord-theme elpy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
