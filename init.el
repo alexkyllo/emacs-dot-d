27b80eee5769b5f858ae89f7838e68093dc3729b
Alex Kyllo <alex.kyllo@gmail.com>
2021-04-30 11:13:28 -0700

    init.el improvements

--- init.el
+++ init.el
@@ -28,6 +28,7 @@
   (elpy-enable)
   ;; Enable elpy in a Python mode
   (add-hook 'python-mode-hook 'elpy-mode)
+  (add-hook 'python-mode-hook 'blacken-mode)
   (setq elpy-rpc-backend "jedi")
   ;; Open the Python shell in a buffer after sending code to it
   (add-hook 'inferior-python-mode-hook 'python-shell-switch-to-shell)
@@ -35,20 +36,28 @@
   ;; https://emacs.stackexchange.com/questions/24453/weird-shell-output-when-using-ipython-5
   (setq python-shell-interpreter "jupyter-console")
   (setq python-shell-interpreter-args "--simple-prompt")
+  (setq python-shell-completion-native-enable nil)
   ;; Enable pyvenv, which manages Python virtual environments
   (pyvenv-mode 1)
   ;; Tell Python debugger (pdb) to use the current virtual environment
   ;; https://emacs.stackexchange.com/questions/17808/enable-python-pdb-on-emacs-with-virtualenv
   (setq gud-pdb-command-name "python -m pdb "))
 
-;; python virtualenvs 
+;; notebooks
+(use-package ein :ensure t)
+
+;; python formatting
+(use-package blacken :ensure t)
+
+;; python virtualenvs
 (use-package conda :ensure t)
 (use-package pipenv :ensure t)
 
 ;; language server mode
 (use-package lsp-mode :ensure t)
-(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+(use-package lsp-ivy :ensure t :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :ensure t :commands lsp-treemacs-errors-list)
+(use-package lsp-latex :ensure t)
 (use-package lsp-pyright
   :ensure t
   :hook (python-mode . (lambda ()
@@ -98,7 +107,8 @@
 ;; ivy autocompletion
 (use-package ivy :ensure t)
 (ivy-mode)
-
+(define-key ivy-minibuffer-map (kbd "C-j") 'ivy-immediate-done)
+(define-key ivy-minibuffer-map (kbd "C-m") 'ivy-alt-done)
 ;; magit
 (use-package magit :ensure t)
 
@@ -106,4 +116,28 @@
 (use-package which-key :ensure t)
 (which-key-mode)
 
+; C-n add new lines at the end of buffer
+(setq next-line-add-newlines t)
+; open emacs full screen
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+; Make Emacs highlight paired parentheses
+(show-paren-mode 1)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 (global-linum-mode t)
+(tool-bar-mode -1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(conda-env-home-directory "/home/alex/miniconda3/")
+ '(custom-safe-themes
+   '("37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" default))
+ '(package-selected-packages
+   '(blacken ein lsp-latex lsp-lens lsp-ivy lsp-treemacs which-key treemacs lsp-pyright lsp-mode pipenv conda use-package nord-theme neotree magit ivy elpy all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
